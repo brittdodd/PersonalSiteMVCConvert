47c8878d2109387222b3d3be844374118677de8e
brittdodd <Brittany.R.Dodd@hotmail.com>
2022-11-01 15:00:53 -0500

    Add project files.

---
+++ PersonalSiteMVCConvert/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalSiteMVCConvert.Models;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace PersonalSiteMVCConvert.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IConfiguration _config;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        // ------------------------------------------------ PUT ALL Personal site actions from here down
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult Centriq()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
